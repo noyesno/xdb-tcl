51850c297a1dd7001a319f38a6a6c0222607159e
noyesno <noyesno.net@gmail.com>
2018-10-14 22:43:18 +0800

    support "-bind" and "-filter" for "xdb query"

--- xdb-tcl.vfs/lib/xdb-tcl-0.1/xdb-tcl.tcl
+++ xdb-tcl.vfs/lib/xdb-tcl-0.1/xdb-tcl.tcl
@@ -33,7 +33,7 @@ proc ${NS}::connect {host port} {
   interp alias {} $client {} ${ns}::client $sock
 
   # TODO: use dbcmd from server response
-  set dbcmd "${ns}::dbcmd-$sock"
+  set dbcmd "${ns}::dbcmd@$sock"
   interp alias {} $dbcmd {} ${ns}::dbcmd $sock $client
 
   $client set -print 0
@@ -41,10 +41,58 @@ proc ${NS}::connect {host port} {
   return $dbcmd
 }
 
+proc ${NS}::filter_argv {argv argwant {argkeys ""}} {
+    set kargv [list]
+    set argv_left [list]
+
+    for { set argidx 0 ; set argc [llength $argv] } {$argidx<$argc} {incr argidx} {
+      set arg [lindex $argv $argidx]
+      if {$arg in $argwant} {
+        lappend kargv $arg [lindex $argv [incr argidx]]
+      } else {
+        lappend argv_left $arg
+      }
+    }
+
+    foreach key $argkeys value $argv_left {
+      if {$key ne "" && $key in $argwant} {
+        lappend kargv $key $value
+      }
+    }
+
+    return $kargv
+}
+
+proc ${NS}::parse_argv {argv argwant {argkeys ""}} {
+    set kargs [dict create]
+    set argv_left [list]
+
+    for { set argidx 0 ; set argc [llength $argv] } {$argidx<$argc} {incr argidx} {
+      set arg [lindex $argv $argidx]
+      if {$arg in $argwant} {
+        dict set kargs $arg [lindex $argv [incr argidx]]
+      } else {
+        lappend argv_left $arg
+      }
+    }
+
+    foreach key $argkeys value $argv_left {
+      if {$key ne "" && $key in $argwant} {
+        dict set kargs $key $value
+      }
+    }
+
+    foreach key $argwant {
+      dict append kargs $key  ;# create if missing
+    }
+
+    return $kargs
+}
+
 proc ${NS}::dbcmd {sock client args} {
 
 
-  set cmd [lindex $args 0]
+  set cmd_argv [lassign $args cmd]
 
   if {$cmd eq "-print"} {
     $client set -print 1
@@ -59,19 +107,30 @@ proc ${NS}::dbcmd {sock client args} {
     return
   }
 
+  set req_argv $cmd_argv
 
+  if {$cmd eq "query"} {
+    set req_argv [filter_argv $cmd_argv {-filter -bind -sql} {-sql -body}]
+  }
 
-  request $sock $args
+  request $sock [list $cmd {*}$req_argv]
 
   # TODO: check eof
   set size   [gets $sock]
-  set result [read $sock $size]
+  set req_result [read $sock $size]
   gets $sock
 
-  if {[$client get -print]} {
-    puts $result
+  set cmd_result $req_result
+
+  if {$cmd eq "query"} {
+    set req_kargs [parse_argv $cmd_argv {-as}]
+    set cmd_result [format_query_result [dict get $req_kargs -as] $cmd_result]
   }
-  return $result
+
+  if {[$client get -print]} {
+    puts $cmd_result
+  }
+  return $cmd_result
 }
 
 proc ${NS}::request {sock body} {
@@ -150,15 +209,10 @@ proc ${NS}::db_open {dbfile} {
 proc ${NS}::db_read_cache {dbcmd args} {
 }
 
-proc ${NS}::db_query {dbcmd args} {
+proc ${NS}::do_db_query {dbcmd args} {
   variable dbcache
 
-  set args [lassign $args sql]
-
-  debug "dbcmd = $dbcmd , sql = $sql"
-
-
-  set dbfile [dict get $dbcache dbcmd $dbcmd  dbfile]
+  set dbfile [dict get $dbcache dbcmd  $dbcmd  dbfile]
   set mtime  [dict get $dbcache dbfile $dbfile mtime]
 
   if {[file mtime $dbfile] > $mtime} {
@@ -166,39 +220,108 @@ proc ${NS}::db_query {dbcmd args} {
     dict unset dbcache query $dbfile
   }
 
+  array set kargs [parse_argv $args {-sql -bind -filter}]
 
-  set argc [llength $args]
+  set cachekey [list $kargs(-sql) $kargs(-bind) $kargs(-filter)]
 
-  set -bind ""
-  set -key [list $sql]
-  if {$argc==1 || $argc==3} {
-    set -bind  [lindex $args end]
-    set args   [lrange $args 0 end-1]
-    lappend -key ${-bind}
-  }
-
-  if {[dict exist $dbcache query $dbfile ${-key}]} {
-    set result [dict get $dbcache query $dbfile ${-key}]
+  if {[dict exist $dbcache query $dbfile $cachekey]} {
+    set result [dict get $dbcache query $dbfile $cachekey]
     debug "query result from cache"
     return $result
   }
 
-  if {[llength $args]==0} {
-    debug "query without bind"
-    dict with -bind {
-      set result [$dbcmd eval $sql]
-    }
-  } elseif {[llength $args]==2} {
-    debug "query with bind [set -bind]"
-    set result ""
-    dict with -bind {
-      $dbcmd eval $sql {*}$args
-    }
-  } else {
-    # TODO: Error
+  set result [db_query $dbcmd $kargs(-sql) $kargs(-bind) $kargs(-filter)]
+
+  dict set dbcache query $dbfile $cachekey $result
+
+  return $result
+}
+
+proc ${NS}::db_query {dbcmd args} {
+  variable dbcache
+
+  lassign $args kargs(-sql) kargs(-bind) kargs(-filter)
+
+   set result [list]
+   set header [list]
+   dict with kargs(-bind) {
+     set nrow 0
+     $dbcmd eval $kargs(-sql) row {
+       switch -- [catch $kargs(-filter) record] {
+         0 { # ok
+           incr nrow
+           if {$nrow==1} {
+             set header $row(*)
+             lappend result $header
+           }
+           set record [list]
+           foreach col $header {
+             lappend record [set row($col)]
+           }
+           lappend result $record
+         }
+         1 { # error
+           return -code error $record
+         }
+         2 { # return
+           lappend result $record
+         }
+         3 { # break
+           break
+         }
+         4 { # continue
+           continue
+         }
+       }
+     }
+   }
+
+   return $result
+}
+
+proc ${NS}::format_query_result {as records} {
+
+  set result $records
+
+  if {$as eq "" || $as eq "table"} {
+    return $result
   }
 
-  dict set dbcache query $dbfile ${-key} $result
+  set nrow 0
+  set header ""
+  set result ""
+  foreach record $records {
+    incr nrow
+    if { $nrow==1 } {
+      set header $record
+      continue
+    }
+
+    switch -- $as {
+      "flat" {
+        # [
+        #   $row_1_col_1, $row_1_col_2,
+        #   $row_2_col_1, $row_2_col_2
+        # ]
+        lappend result {*}$record
+      }
+      "list" {
+        # [
+        #   [$row_1_col_1, $row_1_col_2],
+        #   [$row_2_col_1, $row_2_col_2]
+        # ]
+        lappend result $record
+      }
+      "dict" {
+        set newrec [dict create]
+        foreach col $header val $record {
+          dict set newrec $col $val
+        }
+        lappend result $newrec
+      }
+    }
+  }
+
   return $result
 }
 
@@ -208,7 +331,8 @@ proc ${NS}::execute {client args} {
 
   set ns [namespace current]
 
-  set cmd [lindex $args 0]
+  set cmd_argv [lassign $args cmd]
+
   if {$cmd eq "use"} {
     set dbfile [lindex $args 1]
 
@@ -235,11 +359,10 @@ proc ${NS}::execute {client args} {
   }
 
   if {$cmd eq "query"} {
-    set args [lrange $args 1 end]
+    set dbcmd [$client get dbcmd]
+    set result [do_db_query $dbcmd {*}$cmd_argv]
   }
 
-  set dbcmd [$client get dbcmd]
-  set result [db_query $dbcmd {*}$args]
 
   return [reply $client $result]
 }
@@ -287,10 +410,9 @@ proc ${NS}::read_command {client} {
     }
   }
 
-  debug "size = $size"
   set data [read $sock $size]
   gets $sock
-  debug $size $data
+  debug "request = $size $data"
   execute $client {*}$data
 }
 
